1c4a2907287f9b72c493c599659738fcfaf98a05
baarayy <albaraa.ah26@icloud.com>
2024-08-16 17:45:26 +0300

    Core: add the base entity class

--- src/main/java/com/example/meme/models/BaseEntity.java
+++ src/main/java/com/example/meme/models/BaseEntity.java
@@ -1,6 +1,28 @@
 package com.example.meme.models;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.io.Serializable;
+import java.time.Instant;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Data
 public class BaseEntity implements Serializable {
+    @CreatedDate
+    @Column(name="created_at" ,nullable = false)
+    @JsonIgnore
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name="lastModified_at")
+    @JsonIgnore
+    private Instant lastModifiedAt;
 }
