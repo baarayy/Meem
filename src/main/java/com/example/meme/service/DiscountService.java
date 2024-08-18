a207759b88200802d514671544da18bc719be145
baarayy <albaraa.ah26@icloud.com>
2024-08-18 20:42:49 +0300

    feat: Create discount service

--- src/main/java/com/example/meme/service/DiscountService.java
+++ src/main/java/com/example/meme/service/DiscountService.java
@@ -1,4 +1,65 @@
 package com.example.meme.service;
 
+import com.example.meme.dto.DiscountDTO;
+import com.example.meme.exception.EntityNotFoundException;
+import com.example.meme.models.Discount;
+import com.example.meme.repositories.DiscountRepo;
+import com.example.meme.repositories.ProductRepo;
+import com.example.meme.utils.mappers.DiscountMapper;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DiscountService {
+    private final DiscountRepo repo;
+    private final ProductRepo productRepo;
+    private final DiscountMapper mapper;
+
+    public Page<DiscountDTO> findAll(int page ,int size) {
+        return  repo.findAll(PageRequest.of(page , size)).map(mapper::toDTO);
+    }
+
+    public DiscountDTO findById(Integer id) {
+        return repo.findById(id).map(mapper::toDTO).orElseThrow(()->
+                new EntityNotFoundException("There is no discount with id " + id));
+    }
+
+    @Transactional
+    public DiscountDTO create(DiscountDTO x) {
+        var  d = mapper.toEntity(x);
+        var saved = repo.save(d);
+        return mapper.toDTO(saved);
+    }
+
+    @Transactional
+    public DiscountDTO update(Integer id,DiscountDTO x) {
+        var d = repo.findById(id).orElseThrow(()->
+                new EntityNotFoundException("There is no discount with id " + id));
+        d.setActive(x.active());
+        d.setDesc(x.desc());
+        d.setName(x.name());
+        d.setPercent(x.percent());
+        var list = x.productIds();
+        if(list != null) {
+            d.setProducts(productRepo.findAllById(list));
+        }
+        var saved = repo.save(d);
+        return mapper.toDTO(saved);
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        var d = repo.findById(id);
+        if(d.isPresent()) {
+            var dd = d.get();
+            var list = dd.getProducts();
+            list.forEach(dd::removeProduct);
+            productRepo.saveAll(list);
+            repo.delete(dd);
+        }
+    }
 }
