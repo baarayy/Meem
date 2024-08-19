b652f5cc272342fc02f8b610c7d5a229f2418b50
baarayy <albaraa.ah26@icloud.com>
2024-08-19 19:26:58 +0300

    feat: Add user address service

---
+++ src/main/java/com/example/meme/service/UserAddressService.java
@@ -0,0 +1,61 @@
+package com.example.meme.service;
+
+import com.example.meme.dto.UserAddressDTO;
+import com.example.meme.exception.EntityNotFoundException;
+import com.example.meme.models.UserAddress;
+import com.example.meme.repositories.AddressRepo;
+import com.example.meme.repositories.UserRepo;
+import com.example.meme.utils.mappers.AddressMapper;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserAddressService {
+    private final AddressRepo repo;
+    private final UserRepo userRepo;
+    private final AddressMapper mapper;
+
+    @Transactional
+    public UserAddressDTO create(UserAddressDTO x) {
+        var address = mapper.toEntity(x);
+        var saved = repo.save(address);
+        return mapper.toDTO(saved);
+    }
+
+    public UserAddressDTO findById(Integer id) {
+        return repo.findById(id).map(mapper::toDTO).orElseThrow(()->
+                new EntityNotFoundException("There is no address with id " + id));
+    }
+
+    public Page<UserAddressDTO> findAll(int page ,int size) {
+        return repo.findAll(PageRequest.of(page , size)).map(mapper::toDTO);
+    }
+
+    @Transactional
+    public UserAddressDTO update(Integer id ,UserAddressDTO x) {
+        var address = repo.findById(id).orElseThrow(()->
+                new EntityNotFoundException("There is no address with id " + id));
+        address.setAddressLine1(x.addressLine1());
+        address.setAddressLine2(x.addressLine2());
+        address.setCity(x.city());
+        address.setCountry(x.country());
+        address.setPostalCode(x.postalCode());
+        userRepo.findById(x.userId()).ifPresent(address::setUser);
+        var saved = repo.save(address);
+        return mapper.toDTO(saved);
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        repo.findById(id).ifPresent(repo::delete);
+    }
+
+    public UserAddressDTO findAddressByUserId(Integer id) {
+        return repo.findByUserId(id).map(mapper::toDTO).orElseThrow(()->
+                new EntityNotFoundException("There is no address with user id " + id));
+    }
+}
