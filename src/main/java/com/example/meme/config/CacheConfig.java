57cd5418461400af29182c4265cf0042341d9b9a
baarayy <albaraa.ah26@icloud.com>
2024-08-19 21:11:22 +0300

    feat: add redis configuration

---
+++ src/main/java/com/example/meme/config/CacheConfig.java
@@ -0,0 +1,52 @@
+package com.example.meme.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+@Configuration
+@EnableCaching
+public class CacheConfig implements CachingConfigurer {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    public CacheConfig(RedisConnectionFactory redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory;
+    }
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+    @Override
+    public CacheResolver cacheResolver() {
+        return null;
+    }
+
+    @Override
+    public KeyGenerator keyGenerator() {
+        return null;
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return null;
+    }
+}
