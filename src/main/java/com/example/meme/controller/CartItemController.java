fe9e5f6742472eac6b81ca934b83a55130b5dd50
baarayy <albaraa.ah26@icloud.com>
2024-08-20 23:49:25 +0300

    feat: Add cart item controller

---
+++ src/main/java/com/example/meme/controller/CartItemController.java
@@ -0,0 +1,81 @@
+package com.example.meme.controller;
+
+import com.example.meme.dto.CartItemDTO;
+import com.example.meme.exception.EntityNotFoundException;
+import com.example.meme.service.CartItemService;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cartitems")
+@Validated
+@CrossOrigin(origins = "http://localhost:8080")
+@RequiredArgsConstructor
+public class CartItemController {
+    private final CartItemService service;
+
+    @GetMapping
+    public ResponseEntity<Page<CartItemDTO>> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        var result = service.findAll(page , size);
+        if(result.isEmpty())
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CartItemDTO> findById(@PathVariable Integer id) {
+        var result = service.findById(id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<CartItemDTO> create(@Valid @RequestBody CartItemDTO x) {
+        var createdItem = service.create(x);
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdItem);
+        } catch (ConstraintViolationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CartItemDTO> update(@PathVariable Integer id , @Valid @RequestBody CartItemDTO x) {
+        var updatedItem = service.update(id , x);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(updatedItem);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        try {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch(EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
+
+}
